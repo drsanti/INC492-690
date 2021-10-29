7ad323a7d701e60fa763972ec4c4b8a5c4471943
drsanti <santi.inc.kmutt@gmail.com>
2021-10-29 08:28:50 +0700

    update

---
+++ embedded/examples/ex06_ModbusRTU_CRC/lrc.c
@@ -0,0 +1,11 @@
+#include "ecc.h"
+uint8_t ModbusLRC (uint8_t *pData, uint16_t length)
+{
+    uint8_t lrc = 0 ;
+    int16_t i;
+    for (i = 0; i < length; i++)
+    {
+        lrc += *pData++;
+    }
+    return (uint8_t)(-lrc);
+} 
